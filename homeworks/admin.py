ae51f38e7017b322612a8418a56601c5fba79073
Akmal <akmal9999007@gmail.com>
2025-12-04 23:20:27 +0500

    removed homewrosk

--- homeworks/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Homework
-
-@admin.register(Homework)
-class HomeworkAdmin(admin.ModelAdmin):
-    list_display = ('title', 'due_time', 'created_at')
-    search_fields = ('title', 'description')
-    list_filter = ('created_at',)
